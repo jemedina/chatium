e2898fee30681202d15ac9b8933eb27e062d7ff8
Zorek9502 <zorek_ltc@hotmail.com>
2018-11-11 17:07:46 -0600

    Barra mensajes + boton de enviar

--- client/src/app/components/chat/chat.component.ts
+++ client/src/app/components/chat/chat.component.ts
@@ -1,26 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup } from "@angular/forms";
+
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
+
+
 export class ChatComponent implements OnInit {
+  forma: FormGroup;
   friendId: string;
-  
+
   mockup_user: any = {
     name: "User",
     profile_pic: "assets/images/default_profile_pic.png",
-    status:true
+    status: true
   }
 
+  mockup_mensaje: {
+    texto: string,
+    emisor: string,
+    receptor: string
+  }
+
+ 
+  mensajes_enviados =[]
 
   constructor(private router: ActivatedRoute) {
-    router.params.subscribe( map => this.friendId = map.friendId);
+    router.params.subscribe(map => this.friendId = map.friendId);
   }
 
   ngOnInit() {
+    this.forma = new FormGroup({
+      'mensaje': new FormControl()
+    })
   }
+
+
+  enviar() {
+    if (this.forma.controls.mensaje.value) {
+      this.mockup_mensaje = {
+        texto: this.forma.controls.mensaje.value,
+        emisor: "user",
+        receptor: this.friendId,
+      }
+      console.log(this.mockup_mensaje)
+
+      this.mensajes_enviados.push(this.mockup_mensaje)
+      this.forma.reset();
+      console.log(this.mensajes_enviados)
+    }
+
+
+    if(this.mensajes_enviados.length<1){
+      console.log("hay mensajes")
+    }
+  }
+
+
 
 }
